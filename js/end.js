1f1fe901e0f8686ea02def2eaf3b2c1473ad2252
jncheng <jncheng@sfu.ca>
2015-01-28 18:55:45 -0800

    Updated File

--- js/end.js
+++ js/end.js
@@ -1,15 +1,15 @@
 /** Application popup page component. */
-var PagePopUpContent = React.createClass({
-  displayName: 'PagePopUpContent',
+var PageResultsContent = React.createClass({
+  displayName: 'PageResultsContent',
 
   render: function() {
     return React.DOM.div(null,
-      React.DOM.h2(null, 'Popup'),
-      React.DOM.p(null, 'I have an id of "popup" on my page container and only look like a dialog because the link to me had a ',
-        React.DOM.code(null, 'data-rel="dialog"'),
-        ' attribute which gives me this inset look and a ',
-        React.DOM.code(null, 'data-transition="pop"'),
-        ' attribute to change the transition to pop. Without this, I\'d be styled as a normal page.'),
+      React.DOM.h2(null, 'Results'),
+      React.DOM.p(null, 'You got 75%, Good job. These are the answers that you got right and wrong'),
+      React.DOM.p(null, 'Question 1 wrong, right answer...'),
+      React.DOM.p(null, 'Question 2 right, answer...'),
+      React.DOM.p(null, 'Question 3 right, answer...'),
+      React.DOM.p(null, 'Question 4 right, answer...'),
       JQueryMobileButton({href:'#home'}, 'Show page "home"')
     );
   }
--- js/page2.js
+++ js/page2.js
@@ -7,28 +7,16 @@ var PageTwoContent = React.createClass({
 
   render: function() {
     return React.DOM.div(null,
-      React.DOM.h2(null, 'Two'),
-      React.DOM.p(null,
-        'I have an ',
-        React.DOM.code(null, 'id'),
-        ' of "two" on my page container. I\'m first in the source order so I\'m shown when the page loads.'
-      ),
-      React.DOM.p(null, 'This is a multi-page boilerplate template that you can copy to build your first jQuery Mobile page. This template contains multiple "page" containers inside, unlike a single page template that has just one page within it.'),
-      React.DOM.p(null, 'Just view the source and copy the code to get started. All the CSS and JS is linked to the jQuery CDN versions so this is super easy to set up. Remember to include a meta viewport tag in the head to set the zoom level.'),
-      React.DOM.p(null,
-        'You link to internal pages by referring to the ',
-        React.DOM.code(null, 'id'),
-        ' of the page you want to show. For example, to ',
-        React.DOM.a({href:'#three'}, 'link'),
-        ' to the page with an ',
-        React.DOM.code(null, 'id'),
-        ' of "three", my link would have a ',
-        React.DOM.code(null, 'href="#three"'),
-        ' in the code.'
-      ),
-      React.DOM.h3(null, 'Show internal pages:'),
-      JQueryMobileButton({href:'#three'}, 'Show page "three"'),
-      JQueryMobileButton({href:'#popup', 'data-rel':'dialog', 'data-transition':'pop'}, 'Show page "popup" (as a dialog)')
+      React.DOM.h2(null, 'Example Question'),
+      React.DOM.p(null, 'This is a sample question'),
+      React.DOM.p(null, 'There will be 4 answers, but only 1 will be correct'),
+      React.DOM.p(null, 'Answer 1'),
+      React.DOM.p(null, 'Answer 2'),
+      React.DOM.p(null, 'Answer 3'),
+      React.DOM.p(null, 'Answer 4'),
+      React.DOM.h3(null, ''),
+      JQueryMobileButton({href:'#three'}, 'Next Question'),
+      JQueryMobileButton({href:'#one'}, 'Last Question')
     );
   }
 });
