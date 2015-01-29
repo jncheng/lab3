c19beb037ac606eb8b2172012ab0861061441dfa
jncheng <jncheng@sfu.ca>
2015-01-28 18:55:01 -0800

    Updated file

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ var App = React.createClass({
       JQueryMobilePage({id:'two'}, PageTwoContent(null)),
       JQueryMobilePage({id:'three'}, PageThreeContent(null)),
       JQueryMobilePage({id:'four'}, PageFourContent(null)),
-      JQueryMobilePage({id:'popup', headerTheme:'b'}, PagePopUpContent(null))
+      JQueryMobilePage({id:'results', headerTheme:'b'}, PageResultsContent(null))
     );
   }
 });
@@ -73,7 +73,7 @@ var JQueryMobileFooter = React.createClass({
 
   render: function() {
     return React.DOM.div({'data-role':'footer'},
-      React.DOM.h4(null, 'Page footer')
+      React.DOM.h4(null, 'Page footerr')
     );
   }
 });
@@ -113,27 +113,11 @@ var PageHomeContent = React.createClass({
   render: function() {
     return React.DOM.div(null,
       React.DOM.h2(null, 'Home'),
-      React.DOM.p(null,
-        'I have an ',
-        React.DOM.code(null, 'id'),
-        ' of "home" on my page container. I\'m first in the source order so I\'m shown when the page loads.'
-      ),
-      React.DOM.p(null, 'This is a multi-page boilerplate template that you can copy to build your first jQuery Mobile page. This template contains multiple "page" containers inside, unlike a single page template that has just one page within it.'),
-      React.DOM.p(null, 'Just view the source and copy the code to get started. All the CSS and JS is linked to the jQuery CDN versions so this is super easy to set up. Remember to include a meta viewport tag in the head to set the zoom level.'),
-      React.DOM.p(null,
-        'You link to internal pages by referring to the ',
-        React.DOM.code(null, 'id'),
-        ' of the page you want to show. For example, to ',
-        React.DOM.a({href:'#one'}, 'link'),
-        ' to the page with an ',
-        React.DOM.code(null, 'id'),
-        ' of "two", my link would have a ',
-        React.DOM.code(null, 'href="#two"'),
-        ' in the code.'
-      ),
+      React.DOM.p(null, 'This is a quiz to help you determine how prepared you are for an earthquake.'),
+      React.DOM.p(null, ' An earthquake is inevitable in the near future'),
+      React.DOM.p(null, 'Follow along and answer the questions, your result will be shown once you finish the quiz.'),
       React.DOM.h3(null, 'Show internal pages:'),
-      JQueryMobileButton({href:'#one'}, 'Show page "one"'),
-      JQueryMobileButton({href:'#popup', 'data-rel':'dialog', 'data-transition':'pop'}, 'Show page "popup" (as a dialog)')
+      JQueryMobileButton({href:'#one'}, 'Begin quiz')
     );
   }
 });
